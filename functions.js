2bf827f87625159d258c2d4ba2471f9f745a4fe8
Seun Joshua <sseun976@gmail.com>
2022-03-03 16:16:33 +0000

    first javascript code

---
+++ functions.js
@@ -0,0 +1,28 @@
+// console.log("Hello world");
+
+// // creating a function
+// function getName(name){
+//     console.log(name);
+// } 
+// getName("dele")
+
+function checkHeight(name,height){
+    return height =>"4ft" && name + "is close to the ground" 
+
+}
+
+console.log(checkHeight("lekan","5ft"))
+
+const multiplier =(number) =>{
+    for (let i=0; i<12; i++){
+        console.log(number+"*"+i+"="+(number*i))
+    }
+
+}
+
+function multiplication(afunction){
+    let x = 2
+    afunction(x)
+    
+}
+multiplication(multiplier)
